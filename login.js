3b8949f9401ffea9cb2b26ddc4a415be3f04e852
Rahul Gaur <97507317+RahulGaurr@users.noreply.github.com>
2022-03-13 19:25:55 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+document.querySelector("form").addEventListener("submit", signin);
+
+  var regdUsers = JSON.parse(localStorage.getItem("userDataBase"));
+
+  function signin(event) {
+    event.preventDefault();
+    var pass = document.querySelector("#pass").value;
+    var user = document.querySelector("#user").value;
+
+    for (var i = 0; i < regdUsers.length; i++) {
+      console.log(regdUsers[i].userName, regdUsers[i].passwd);
+      if (regdUsers[i].userName == user && regdUsers[i].passwd == pass) {
+        window.location.href = "home.html";
+      }
+    }
+  }
